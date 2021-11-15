2bd1050c63b7aee2e8e5137488d03a69ce4eac27
David Lancheros <davidlancherosg@gmail.com>
2021-11-15 16:45:13 +0100

    carte complex et form

---
+++ client/src/components/CreateForm.jsx
@@ -0,0 +1,93 @@
+import { faHardHat } from "@fortawesome/free-solid-svg-icons";
+import React, { Component } from "react";
+import APIHandler from "../api/handler.js";
+
+export default class CreateForm extends Component {
+  // using the constructor form to associate a ref
+  constructor(props) {
+    console.log(props);
+    super(props); // MANDATORY !!!!
+    this.state = {
+      title: "",
+      difficulty: "",
+      ingredients: [],
+      quantities: [],
+      image: React.createRef(), // create a reference to attach to the virtual DOM
+    };
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault(); // prevent the form to reload
+    // destructuring the state
+    const { title, difficulty, ingredients, quantities } = this.state;
+    // accessing the image out of the ref
+    const file = this.state.image.current.files[0]; // target the image file associated to the input[type=file]
+
+    const uploadData = new FormData(); // create a form data => an object to send as post body
+
+    // appending the keys / values pairs to the FormData
+    uploadData.append("title", title); // create a key [name] on the formDate
+    uploadData.append("difficulty", difficulty); // create a key [age] on the formDate
+    uploadData.append("ingredients", ingredients); // create a key [color] on the formDate
+    uploadData.append("quantities", quantities);
+    uploadData.append("image", file);
+
+    try {
+      await APIHandler.post("/", uploadData);
+      console.log("this is this props", this.props);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  
+
+  render() {
+    console.log("this is state", this.state.difficulty);
+    return (
+      <form>
+        <h1>create your own recipe!</h1>
+        <input
+          name="title"
+          type="text"
+          placeholder="title"
+          value={this.state.title}
+          onChange={this.handleChange}
+        />
+        <select
+          name="difficulty"
+          value={this.state.difficulty}
+          onChange={this.handleChange}
+          className="input"
+        >
+          <option value="easy">Easy</option>
+          <option selected value="medium">Medium</option>
+          <option value="difficult">difficult</option>
+        </select>
+        <input
+          name="ingredients"
+          type="text"
+          placeholder="ingredients"
+          value={this.state.ingredients}
+          onChange={this.handleChange}
+        />
+        <input
+          name="quantities"
+          type="text"
+          placeholder="quantities"
+          value={this.state.quantities}
+          onChange={this.handleChange}
+        />
+        {/* THE REF IS HERE */}
+        <input ref={this.state.image} name="image" type="file" />
+        <button onClick={this.handleSubmit}>ok</button>
+      </form>
+    );
+  }
+}
--- client/src/views/recipe.jsx
+++ client/src/views/recipe.jsx
@@ -2,79 +2,58 @@ import React, { useState, useEffect } from "react";
 import NavigationArrow from "./../components/NavigationArrow";
 import "./../styles/ingredients.css";
 import APIHandler from "../api/handler";
+import SearchIngredients from "../components/SearchIngredients";
+import AllRecipes from "./AllRecipes";
 
 function Recipe(props) {
   const [recipe, setRecipe] = useState([]);
-  console.log(props);
   // Similar a componentDidMount y componentDidUpdate:
+
   useEffect(() => {
-    APIHandler.get("/api/all-recipes/" + props.match.params.id)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  });
+    fetchRecipes();
+  }, []);
+
+  const fetchRecipes = async () => {
+    try {
+      const res = await APIHandler.get("/all-recipes/" + props.match.params.id);
+      setRecipe(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  console.log("this is the", recipe);
+
 
   return (
-  <div>
-  p
-  </div>
-  )
-  
+    <>
+      <div className="shadow-drop-2-center wrapper">
+        <div>
+          <img src={recipe.image} alt={recipe.title} />
+          <h1>{recipe.title}</h1>
+          <p>⭐️⭐️⭐️⭐️⭐️</p>
+        </div>
+        <div className="difficulties">
+          <h2>{recipe.difficulty}</h2>
+        </div>
+        <div className="wrapper-ingredients">
+          {recipe.ingredients?.map((product, i) => {
+            return (
+              <div className="wrapp-ingredient shadow-drop-2-center ">
+                <h3>{product}</h3>
+                <p>{recipe.quantities[i]}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          <p>{recipe.instructions}</p>
+        </div>
+        <div>
+          <h2>Comments</h2>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Recipe;
-
-// export default class Recipe extends Component {
-
-//   render() {
-
-//     console.log("this is api", APIHandler)
-//     console.log("this is the console", this.props)
-//     return (
-//       <>
-//         <div className="shadow-drop-2-center wrapper">
-//           <div>
-//             <img src="./../../téléchargement.jpeg" alt="" />
-//             <h1>title</h1>
-//             <p>⭐️⭐️⭐️⭐️⭐️</p>
-//           </div>
-//           <div className="difficulties">
-//             <h2>Easy</h2>
-//           </div>
-//           <div>
-//             <div className="wrapp-ingredients shadow-drop-2-center ">
-//               <img
-//                 className="ingredients"
-//                 src="./../../potato_PNG98094.png"
-//                 alt="lol"
-//               />
-//               <h3>Name</h3>
-//               <p>Quantities</p>
-//             </div>
-//           </div>
-//           <div>
-//             <p>
-//               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam
-//               dignissimos, ad cum quae tenetur dolorum rem? Velit quod atque
-//               doloremque itaque sunt placeat enim sapiente iste, dolores omnis
-//               dolore vitae. Lorem ipsum dolor sit amet consectetur adipisicing
-//               elit. Nostrum, nobis! Impedit atque perferendis itaque nihil
-//               corrupti porro totam praesentium dolorem fugiat optio, velit
-//               cupiditate adipisci eligendi quidem aliquid veniam! Optio? Odit
-//               animi officia sequi repudiandae blanditiis at molestiae
-//               consequuntur praesentium, qui ipsa laboriosam voluptas! Asperiores
-//               delectus at quam temporibus nam cumque tempore praesentium
-//               officiis, dolore omnis molestias architecto, voluptas deleniti.
-//             </p>
-//           </div>
-//           <div>
-//             <h2>Comments</h2>
-//           </div>
-//         </div>
-//       </>
-//     );
-//   }
-// }
